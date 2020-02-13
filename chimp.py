817c26af359beaeca80a9299097ad8d2d4222fc2
Groverkss <groverkss@gmail.com>
2020-02-13 17:45:11 +0530

    Added loader module

--- chimp.py
+++ chimp.py
@@ -1,8 +1,5 @@
-import os
-import sys
 import pygame
-
-from pygame.locals import *
+from loaders import *
 
 # If fonts cannot be loaded, give warning
 if not pygame.font:
@@ -11,43 +8,6 @@ if not pygame.font:
 # If sounds mixer cannot be loaded, give warning
 if not pygame.mixer:
     print("Warning, sound disabled")
-
-# Load image and throw error if image cannot be loaded
-def load_image(name, colorkey = None):
-
-    # Loads image located at ./data/name
-    fullname = os.path.join("data", name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error as message:
-        print("Cannot load image:", name)
-        raise SystemExit(message)
-    image = image.convert()
-
-    # Sets colorkey
-    if colorkey is not None:
-        if colorkey is -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey, RLEACCEL)
-    return image, image.get_rect()
-
-# Load sound and throw error if sound cannot be loaded
-def load_sound(name):
-
-    # If sound mixer not loaded then don't load sound
-    class NoneSound:
-        def play(self): pass
-    if not pygame.mixer:
-        return NoneSound()
-
-    # Loads image located at ./data/name
-    fullname = os.path.join("data", name)
-    try:
-        sound = pygame.mixer.Sound(fullname)
-    except pygame.error as message:
-        print("Cannot load sound:", name)
-        raise SystemError(message)
-    return sound
 
 # Fist which follows the mouse
 class Fist(pygame.sprite.Sprite):
